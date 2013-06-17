8f1cb1619f80e8cdaefff84f9744d86512aef451
Christopher Jackson <himself@cj-jackson.com>
2013-06-17 11:04:05 +0100

    Improved Balencer!

--- balancer.go
+++ balancer.go
@@ -2,61 +2,39 @@ package proxyutil
 
 import (
 	"net/http"
-	"sort"
 	"sync"
 )
 
-type balencerItem struct {
-	handler http.Handler
-	tally   uint8
-}
-
-type balencerItems []*balencerItem
-
-func (bal balencerItems) Len() int {
-	return len(bal)
-}
-
-func (bal balencerItems) Less(i, j int) bool {
-	return bal[i].tally < bal[j].tally
-}
-
-func (bal balencerItems) Swap(i, j int) {
-	bal[i], bal[j] = bal[j], bal[i]
-}
-
-func (bal balencerItems) Reset() {
-	for _, ba := range bal {
-		ba.tally = uint8(0)
-	}
-}
-
 type balencer struct {
 	sync.Mutex
-	items balencerItems
+	handlers []http.Handler
 }
 
 func Balencer(handlers ...http.Handler) http.Handler {
-	bal := balencer{}
+	bal := &balencer{}
 	bal.Lock()
 	defer bal.Unlock()
-	bal.items = balencerItems{}
+	bal.handlers = []http.Handler{}
 
 	for _, handler := range handlers {
-		bal.items = append(bal.items, &balencerItem{handler, uint8(0)})
+		bal.handlers = append(bal.handlers, handler)
 	}
 
 	return bal
 }
 
-func (bal balencer) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+func (bal *balencer) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	bal.Lock()
-	item := bal.items[0]
-	item.tally++
-	sort.Sort(bal.items)
-	if item.tally >= 200 {
-		bal.items.Reset()
+	if len(bal.handlers) < 0 {
+		bal.Unlock()
+		return
 	}
+	item := bal.handlers[0]
+	if len(bal.handlers) <= 1 {
+		goto unlock
+	}
+	bal.handlers = append(bal.handlers[1:], item)
+unlock:
 	bal.Unlock()
-	item.handler.ServeHTTP(res, req)
+	item.ServeHTTP(res, req)
 }
